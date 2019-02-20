bb6c9fdded5fb8f7fdb178546823c01c3b055505
main lab <mlab@usiu.ac.ke>
2019-02-20 11:20:00 +0300

    Home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login_form">
+           <form action="" method="POST">
+             <label for="username">Username</label>
+             <br> <br>
+             <input type="text" name="username" placeholder="Username">
+             <br> <br> 
+             <label for="username">Password</label>
+             <br> <br>
+             <input type="password" name="password" placeholder="Password">
+             <br> <br>
+             <input type="submit" name="loginbtn" value="LOGIN">
+           </form>        
+        </div>
+
+    </div>
+</body>
+</html>
